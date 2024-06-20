5d4a3954a2bc276bf0b68a6b4902408ac82ecf0a
Dan Klinger <dpk25@cornell.edu>
2024-06-20 17:55:55 -0400

    moving app removal to bootstrap

--- scripts/avd/ccss/bootstrap.ps1
+++ scripts/avd/ccss/bootstrap.ps1
@@ -9,3 +9,55 @@ Install-Module -Name Microsoft.WinGet.Client -Force
 # Reset Windows Terminal
 Get-AppxPackage Microsoft.WindowsTerminal | Reset-AppPackage
 
+# Install Basic WinGet Packages
+
+# Remove Windows Bloatware
+##Get appx Packages
+$Packages = Get-AppxPackage
+
+##Create Your allowlist
+$AllowList = @(
+    '*WindowsCalculator*',
+    '*Office.OneNote*',
+    '*Microsoft.net*',
+    '*MicrosoftEdge*',
+    '*WindowsStore*',
+    '*WindowsTerminal*',
+    '*WindowsNotepad*',
+    '*Paint*',
+    '*Microsoft.PowerAutomateDesktop*',
+    '*Microsoft.CompanyPortal*',
+    '*Microsoft.WindowsMaps*',
+    '*Microsoft.Windows.Photos*',
+    '*Microsoft.HEIFImageExtension*',
+    '*Microsoft.HEVCVideoExtension*',
+    '*Microsoft.RawImageExtension*',
+    '*Microsoft.VP9VideoExtensions*',
+    '*Microsoft.WebMediaExtensions*',
+    '*Microsoft.WebpImageExtension*'
+)
+
+###Get All Dependencies
+ForEach($Dependency in $AllowList){
+    (Get-AppxPackage  -Name “$Dependency”).dependencies | ForEach-Object{
+        $NewAdd = "*" + $_.Name + "*"
+        if($_.name -ne $null -and $AllowList -notcontains $NewAdd){
+            $AllowList += $NewAdd
+       }
+    }
+}
+
+##View all applications not in your allowlist
+ForEach($App in $Packages){
+    $Matched = $false
+    Foreach($Item in $AllowList){
+        If($App -like $Item){
+            $Matched = $true
+            break
+        }
+    }
+    ###Nonremovable attribute does not exist before 1809, so if you are running this on an earlier build, remove “-and $app.NonRemovable -eq $false” rt; it attempts to remove everything
+    if($matched -eq $false -and $app.NonRemovable -eq $false){
+        Get-AppxPackage -AllUsers -Name $App.Name -PackageTypeFilter Bundle | Remove-AppxPackage -AllUsers
+    }
+}
