6021d1e828ac0982cd88284bb13afce850913fd8
Dan Klinger <dpk25@cornell.edu>
2024-07-10 17:12:22 -0400

    msix app dependencies and accept flags for winget

--- scripts/avd/ccss/bootstrap.ps1
+++ scripts/avd/ccss/bootstrap.ps1
@@ -5,28 +5,31 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 # Install WinGet
 Install-Module -Name Microsoft.WinGet.Client -Force
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 
 # Reset Windows Terminal
-Get-AppxPackage Microsoft.WindowsTerminal | Reset-AppPackage
+Get-AppxPackage Microsoft.WindowsTerminal -AllUsers | Reset-AppPackage
 
 # Reset profile session
 . $profile
 
 # Install Basic WinGet Packages
+# Microsoft.WindowsAppRuntime.1.5
+winget install -e --id Microsoft.WindowsAppRuntime.1.5 --accept-source-agreements --accept-package-agreements
 # 7zip
-winget install -e --id mcmilk.7zip-zstd
+winget install -e --id mcmilk.7zip-zstd --accept-source-agreements --accept-package-agreements
 # Notepad++
-winget install -e --id Notepad++.Notepad++
+winget install -e --id Notepad++.Notepad++ --accept-source-agreements --accept-package-agreements
 # Git
-winget install -e --id Git.Git
+winget install -e --id Git.Git --accept-source-agreements --accept-package-agreements
 # Git LFS
-winget install -e --id GitHub.GitLFS
+winget install -e --id GitHub.GitLFS --accept-source-agreements --accept-package-agreements
 # Python 3.12
-winget install -e --id Python.Python.3.12
+winget install -e --id Python.Python.3.12 --accept-source-agreements --accept-package-agreements
 # TexStudio
-winget install -e --id TeXstudio.TeXstudio
+winget install -e --id TeXstudio.TeXstudio --accept-source-agreements --accept-package-agreements
 # TortoiseSVN
-winget install -e --id TortoiseSVN.TortoiseSVN
+winget install -e --id TortoiseSVN.TortoiseSVN --accept-source-agreements --accept-package-agreements
 
 # Remove Windows Bloatware
 ##Get appx Packages
